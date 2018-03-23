ef14dee26d3639f70ca6c25e6cd4fb48de00f586
Soldy <testa.a.tapos@gmail.com>
2018-03-23 03:31:43 +0000

    array fix

--- index.js
+++ index.js
@@ -11,47 +11,50 @@ exports.json = function () {
             out += this.config.tab;
         return out;
     };
-    this.json = function(json){
-        return that.formater(json, 1);
+    this.json = function (json) {
+        return JSON.stringify(JSON.parse(that.formater(json, 1)));
     }
     this.formater = function (json, level) {
         var out = "";
+        let partType = "object";
         if (Array.isArray(json)) {
-            out +=  "[\n";
+            out += "[\n";
+            partType = "array";
         } else if (typeof json === 'object') {
             out += "{\n";
         }
-        var turn =0;
-            for(var ni in json){
-                if (turn>0) out += ",\n";
-                turn++;
-                out += ""+that.linesep(level)+ni.toString()+" : ";
-                if (typeof json[ni] === 'string') {
-                    out += '"'+json[ni].toString()+'"';    
-                } else if(typeof json[ni] === 'number'){
-                    out += json[ni].toString();       
-                } else if(typeof json[ni] === 'function'){
-                    out += "function(){}";                           
-                } else if(typeof json[ni] === 'boolean'){
-                    if(json[ni] === true){
-                        out += "true"; 
-                    }else{
-                        out += "false";    
-                    }   
-                } else if(typeof json[ni] === 'object'){
-                    out+=that.formater(json[ni], level+1)
-                } 
-                
+        var turn = 0;
+        for (var ni in json) {
+            if (turn > 0)
+                out += ",\n";
+            turn++;
+            if (partType === "object")
+                out += "" + that.linesep(level) + ni.toString() + " : ";
+            if (typeof json[ni] === 'string') {
+                out += '"' + json[ni].toString() + '"';
+            } else if (typeof json[ni] === 'number') {
+                out += json[ni].toString();
+            } else if (typeof json[ni] === 'function') {
+                out += "function(){}";
+            } else if (typeof json[ni] === 'boolean') {
+                if (json[ni] === true) {
+                    out += "true";
+                } else {
+                    out += "false";
+                }
+            } else if ((typeof json[ni] === 'object') || (Array.isArray(json[ni]))) {
+                out += that.formater(json[ni], level + 1)
             }
 
+        }
+
         if (Array.isArray(json)) {
-            out += "\n"+that.linesep(level-1) + "]";
+            out += "\n" + that.linesep(level - 1) + "]";
         } else if (typeof json === 'object') {
-            out += "\n"+that.linesep(level-1) + "}";
+            out += "\n" + that.linesep(level - 1) + "}";
         }
         return out;
     };
-    var that=this;
+    var that = this;
 }
-
 
